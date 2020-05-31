04defab2319ad7f46df44a101eaa2200097e80e6
Matthew Lee <mattlkf@outlook.com>
2020-05-31 16:50:37 -0700

    true bidirectional ping-pong heartbeats

---
+++ demo_pp_a.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bazel-bin/src/client/greeter_client -other_port 50051 -self_port 50052
--- src/client/greeter_client.cc
+++ src/client/greeter_client.cc
@@ -16,29 +16,44 @@
  *
  */
 
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include <grpcpp/grpcpp.h>
 
 #include "src/protos/helloworld.grpc.pb.h"
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
+ABSL_FLAG(std::string, self_ip, "0.0.0.0", "my ip");
+ABSL_FLAG(std::string, self_port, "50051", "my port");
+ABSL_FLAG(std::string, other_ip, "0.0.0.0", "other ip");
+ABSL_FLAG(std::string, other_port, "50052", "other port");
+
 using grpc::Channel;
 using grpc::ClientContext;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
 using grpc::Status;
 using helloworld::Greeter;
 using helloworld::HelloReply;
 using helloworld::HelloRequest;
 
+using namespace std;
+
 class GreeterClient {
- public:
+public:
   GreeterClient(std::shared_ptr<Channel> channel)
       : stub_(Greeter::NewStub(channel)) {}
 
   // Assembles the client's payload, sends it and presents the response back
   // from the server.
-  std::string SayHello(const std::string& user) {
+  std::string SayHello(const std::string &user) {
     // Data we are sending to the server.
     HelloRequest request;
     request.set_name(user);
@@ -63,29 +78,105 @@ class GreeterClient {
     }
   }
 
- private:
+private:
   std::unique_ptr<Greeter::Stub> stub_;
 };
 
-int main(int argc, char** argv) {
-  std::string address = "localhost";
-  std::string port = "50051";
-  std::string server_address = address + ":" + port;
-  std::cout << "Client querying server address: " << server_address << std::endl;
+class GreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext *context, const HelloRequest *request,
+                  HelloReply *reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+
+  Status SayHelloAgain(ServerContext *context, const HelloRequest *request,
+                       HelloReply *reply) override {
+    std::string prefix("Hello again ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+};
+
+// Print Current Time
+void print_time_point(std::chrono::system_clock::time_point timePoint) {
+  std::time_t timeStamp = std::chrono::system_clock::to_time_t(timePoint);
+  std::cout << std::ctime(&timeStamp) << std::endl;
+}
+
+void RunServer() {
+
+  std::string client_ip = absl::GetFlag(FLAGS_self_ip);
+  std::string client_port = absl::GetFlag(FLAGS_self_port);
+  std::string client_address = client_ip + ":" + client_port;
+  std::cout << "Client's own address: " << client_address << std::endl;
+
+  GreeterServiceImpl service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(client_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << client_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+void RunClient() {
+	std::cout << "Running the client" << std::endl;
+  std::string server_ip = absl::GetFlag(FLAGS_other_ip);
+  std::string server_port = absl::GetFlag(FLAGS_other_port);
+  std::string server_address = server_ip + ":" + server_port;
+  std::cout << "Client querying server address: " << server_address
+            << std::endl;
 
   // Instantiate the client. It requires a channel, out of which the actual RPCs
   // are created. This channel models a connection to an endpoint (in this case,
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureChannelCredentials()).
-  GreeterClient greeter(grpc::CreateChannel(
-      server_address, grpc::InsecureChannelCredentials()));
+  GreeterClient greeter(
+      grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
   std::string user("world");
 
-  for(int i=0;i<10;i++) {
-      std::string reply = greeter.SayHello(user);
-      std::cout << "Greeter received: " << reply << std::endl;
+  // Run the "client"
+	while(true) {	
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+
+		// create a time point pointing to 1 second in future
+		std::chrono::system_clock::time_point timePoint =
+		std::chrono::system_clock::now() + std::chrono::seconds(1);
+		std::cout << "Going to Sleep Until :: "; print_time_point(timePoint);
+		// Sleep Till specified time point
+		// Accepts std::chrono::system_clock::time_point as argument
+		std::this_thread::sleep_until(timePoint);
+		std::cout<<"Current Time :: ";
+		// Print Current Time
+		print_time_point(std::chrono::system_clock::now());
   }
+}
+
+int main(int argc, char **argv) {
+  // Parse command line arguments
+  absl::ParseCommandLine(argc, argv);
+
+	print_time_point(std::chrono::system_clock::now());
+
+  // Begin the server in a separate thread
+	std::thread th(&RunServer);
+
+  // Begin the client
+  RunClient();
+
+	// Wait for the server to exit
+	std::cout << "Waiting for the server to exit" << std::endl;
+	th.join();
 
   return 0;
 }
-
